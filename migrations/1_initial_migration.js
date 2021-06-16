983b668629a8e46adab3b297b73ae6566f0057f4
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-16 21:11:28 +0800

    submit

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,13 @@
 const Migrations = artifacts.require("Migrations");
 const LizToken = artifacts.require("LIZToken");
+const LizMiner = artifacts.require("LizMiner");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
 
   deployer.deploy(LizToken); //deploy LizToken
+
+  deployer.deploy(LizMiner);
 
 
 };
