3be810c8e971901b4a8c2ce6df27d4e1c6bf19ab
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-22 05:22:24 +0800

    Update LizMinerP2.test.ts
    add test scripts of setUserLevel & getUserLevel

--- test/LizMinerP2.test.ts
+++ test/LizMinerP2.test.ts
@@ -214,6 +214,50 @@ describe('LizMiner', () => {
 
     });
 
+    it('wallet-->account1-->account2, setUserLevel()', async () => {
+        // Set Parent
+        await instanceLizMiner.SetParentByAdmin(account1.address,wallet.address);
+        await instanceLizMiner.SetParentByAdmin(account2.address,account1.address);
+
+        expect(await instanceLizMiner.getParent(account1.address)).to.equal(wallet.address);
+        expect(await instanceLizMiner.getParent(account2.address)).to.equal(account1.address);
+
+        // transfer LIZ_TRANSFER_AMOUNT to account1
+        await expect(instanceLIZToken.transfer(account1.address, LIZ_TRANSFER_AMOUNT))
+        .to.emit(instanceLIZToken, 'Transfer')
+        .withArgs(wallet.address, account1.address, LIZ_TRANSFER_AMOUNT);
+        // transfer LIZ_TRANSFER_AMOUNT to account2
+        await expect(instanceLIZToken.transfer(account2.address, LIZ_TRANSFER_AMOUNT))
+        .to.emit(instanceLIZToken, 'Transfer')
+        .withArgs(wallet.address, account2.address, LIZ_TRANSFER_AMOUNT);
+
+        // account1 buyVIP(7)
+                
+
+                // Prepare-approve lizToken
+                const instanceLIZToken_fromAccount1 = instanceLIZToken.connect(account1);
+                await Promise.all([
+                    instanceLIZToken_fromAccount1.approve(instanceLizMiner.address, ALLOWANCE_AMOUNT)
+                ]);	
+                const instanceLizMiner_fromAccount1 = instanceLizMiner.connect(account1);
+                await instanceLizMiner_fromAccount1.buyVip(7,{gasLimit:GAS_LIMIT});
+
+        // account2 buyVIP(1)
+
+        // Prepare-approve lizToken
+        const instanceLIZToken_fromAccount2 = instanceLIZToken.connect(account2);
+        await Promise.all([
+            instanceLIZToken_fromAccount2.approve(instanceLizMiner.address, 1000000)
+        ]);	
+        const instanceLizMiner_fromAccount2 = instanceLizMiner.connect(account2);
+        await instanceLizMiner_fromAccount2.buyVip(1,{gasLimit:GAS_LIMIT});
+
+        expect(await instanceLizMiner.getUserLevel(account1.address)).to.equal(7);
+        expect(await instanceLizMiner.getUserLevel(account2.address)).to.equal(1);
+
+
+    });
+
 
     
 });
