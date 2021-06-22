9050bd15c22ec5718f50558162ebed7c6c9f400c
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-22 16:06:08 +0800

    Update LizMiner.js

--- test_truffle/LizMiner.js
+++ test_truffle/LizMiner.js
@@ -1,11 +1,16 @@
 const MetaCoin = artifacts.require("MetaCoin");
 const LizMiner = artifacts.require("LizMiner");
 const LizToken = artifacts.require("LIZToken");
+const LpWallet = artifacts.require("LpWallet");
 const WETH = artifacts.require("BEP20/WETH");
 
 const chalk = require('chalk');
 
-contract("LizMiner test script", async accounts => {
+contract("LizMiner test script", async (accounts,network) => {
+    const account0 = accounts[0];
+    const OWNER = account0;
+    const account1 = accounts[1];
+    const account2 = accounts[2];
 
     it("LizMiner_test001", async () => {
         const instantceLizMiner = await LizMiner.deployed()
@@ -25,6 +30,21 @@ contract("LizMiner test script", async accounts => {
         console.log(chalk.yellow("poolInfo.totaljthash",poolInfo.totaljthash));
         console.log(chalk.yellow("poolInfo.minpct",poolInfo.minpct));
         console.log(chalk.yellow("poolInfo.maxpct",poolInfo.maxpct));
+
+        const poolInfo_poolwallet_address = poolInfo.poolwallet;
+        // console.log(poolInfo_poolwallet_address);
+
+        const instanceLpWallet = await LpWallet.at(poolInfo_poolwallet_address);
+        console.log(chalk.yellow(await instanceLpWallet.getBalance(account0,true)));
+        console.log(chalk.yellow(await instanceLpWallet.getBalance(account0,false)));
+
+        console.log(chalk.yellow(await instanceLpWallet.getBalance(account1,true)));
+        console.log(chalk.yellow(await instanceLpWallet.getBalance(account1,false)));
+
+        console.log(chalk.yellow(await instanceLpWallet.getBalance(account2,true)));
+        console.log(chalk.yellow(await instanceLpWallet.getBalance(account2,false)));
+
+        
     });
 
     it("should put 10000 MetaCoin in the first account", async () => {
