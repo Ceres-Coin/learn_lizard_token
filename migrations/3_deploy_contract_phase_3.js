cb9491ba48517d7891aed11ecd012a03a36b4086
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-23 09:30:36 +0800

    Update 3_deploy_contract_phase_3.js

--- migrations/3_deploy_contract_phase_3.js
+++ migrations/3_deploy_contract_phase_3.js
@@ -190,11 +190,10 @@ module.exports = async function(deployer, network, accounts) {
     ]);
 
 	console.log(chalk.red.bold("========================= INITIAL CONTRACT =================="));
-    await instantceLizMiner.InitalContract(instanceLizToken.address,wethInstance.address,wethInstance.address,wethInstance.address,wethInstance.address,FEE_OWNER);
+    await instantceLizMiner.InitalContract(instanceLizToken.address,pair_instance_LIZ_WETH.address,pair_instance_LIZ_USDC.address,pair_instance_LIZ_USDC.address,pair_instance_LIZ_USDC.address,FEE_OWNER);
     
     console.log(chalk.red.bold("========================= ADD Trading Pool =================="));
-    const addTradingPool_address = wethInstance.address;
-    await instantceLizMiner.addTradingPool(addTradingPool_address,addTradingPool_address,TRADINGPOOL_HASHRATE,TRADINGPOOL_PCTMIN,TRADINGPOOL_PCTMAX);
+    await instantceLizMiner.addTradingPool(wethInstance.address,pair_instance_LIZ_WETH.address,TRADINGPOOL_HASHRATE,TRADINGPOOL_PCTMIN,TRADINGPOOL_PCTMAX);
 
 	
 
