b1c4ff5b459b036d6508817910621a4beb53ea27
Ilya Prigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2021-06-24 07:47:23 +0800

    add yarn test scripts "yarn test2"

--- test_truffle/LizMiner2.test.js
+++ test_truffle/LizMiner2.test.js
@@ -78,6 +78,8 @@ contract("LizMiner test script", async (accounts,network) => {
         }
     });
     it ("check web3.eth.getBlockNumber()", async() => {
-        console.log(chalk.yellow("web3.eth.getBlockNumber(): ",await web3.eth.getBlockNumber()));
+        // console.log(chalk.yellow((new BigNumber(await web3.eth.getBlockNumber())).toNumber()));
+        expect((new BigNumber(await web3.eth.getBlockNumber())).toNumber()).to.gt(3000);
+        
     })
 });
